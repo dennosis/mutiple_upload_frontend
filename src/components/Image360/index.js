19d47abad95feba91df19bfb4b9ce15d6531744c
dennosis <dennisaguiar@hotmail.com>
2019-02-04 01:31:49 -0200

    criação do 360 view

---
+++ src/components/Image360/index.js
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import './image360.css';
+//import { promisify } from "util";
+//import { POINT_CONVERSION_HYBRID } from "constants";
+
+class image360 extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      uploadedFiles: [],
+      curImage:0,
+      numImages:0,
+      isMoveInit:false,
+      ymin:0,
+      ymax:0,
+      xmin:0,
+      xmax:0,
+      coef: 0,
+      positionEnter: 0,
+      imgEnter:0
+    } 
+    this.rotator = React.createRef()
+  }
+
+  async componentWillMount(){
+
+    let tmpUpload = [];
+
+    for(let i = 0; i < 36; i++){
+      tmpUpload[i] = {
+        id: i,
+        url: require(`./images/${i}.jpg`)
+      }
+    }
+    
+    await this.setState({
+      uploadedFiles: tmpUpload,
+      numImages:tmpUpload.length
+    })
+
+    //console.log(this.state.uploadedFiles);
+  }
+
+
+
+  moduleNum = (a, b) => (((a % b) + b ) % b)
+  
+  isMove = async (posX, posY) =>{
+     //console.log(this.state)
+
+    if(
+      this.state.xmin < posX &&
+      this.state.xmax > posX &&
+      this.state.ymin < posY &&
+      this.state.ymax > posY &&
+      this.state.isMoveInit
+    ){
+
+      //valores em relacao ao comeco da div
+      const divPosition = posX - this.rotator.current.offsetLeft
+
+      //valor da posição que esta sendo movida
+      const currentPos = await parseInt((this.state.positionEnter - divPosition) * this.state.coef);
+
+      //valor da imagem na posição que esta sendo movida
+      var currentImg = this.state.imgEnter + currentPos;
+
+      //ajuste com o 'mod' para que currentImg fique entre '0' a '(numImages-1)'
+      currentImg = await this.moduleNum(currentImg, this.state.numImages);
+
+      await this.setState({
+        curImage:currentImg
+      }) 
+    }
+  }
+
+  isMoveInit= async (posX, posY)=>{
+    await this.setState({
+      ymin:this.rotator.current.offsetTop,
+      ymax:this.rotator.current.offsetTop + this.rotator.current.offsetHeight,
+      xmin:this.rotator.current.offsetLeft,
+      xmax:this.rotator.current.offsetLeft + this.rotator.current.offsetWidth,
+      coef: this.state.numImages / this.rotator.current.offsetHeight,
+      positionEnter: posX - this.rotator.current.offsetLeft,
+      imgEnter: this.state.curImage,
+      isMoveInit:true
+    })
+
+
+  }
+
+  mouseMove = async (e)=>{
+   const posX = e.pageX
+   const posY = e.pageY
+   await this.isMove(posX, posY)
+  }
+
+  mouseDown = async (e) =>{
+    const posX = e.pageX
+    const posY = e.pageY
+    await this.isMoveInit(posX, posY)
+  }
+
+  mouseUp = async () =>{
+    await this.setState({
+      isMoveInit:false
+    })
+  }
+
+  mouseLeave = async () =>{
+    await this.setState({
+      isMoveInit:false
+    })
+  }
+  
+
+
+  touchMove = async (e) =>{
+    const posX = e.changedTouches[0].pageX
+    const posY = e.changedTouches[0].pageY
+    await this.isMove(posX, posY)
+  }
+
+  touchStart = async (e) =>{
+    const posX = e.changedTouches[0].pageX
+    const posY = e.changedTouches[0].pageY
+    await this.isMoveInit(posX, posY)
+  }
+
+  touchEnd = async () =>{
+    await this.setState({
+      isMoveInit:false
+    })
+  }
+  
+
+
+  render() {
+  const curImage = this.state.uploadedFiles[this.state.curImage].url
+    return (
+      <div draggable="false" id="rotator" className="none" 
+        ref={this.rotator}
+        onMouseMove = {this.mouseMove}
+        onMouseLeave = {this.mouseLeave}
+        onMouseDown = {this.mouseDown}
+        onMouseUp = {this.mouseUp}
+        
+        onTouchMove = {this.touchMove}
+        onTouchStart = {this.touchStart}
+        onTouchEnd = {this.touchEnd}
+      >
+    
+		      <img draggable="false" src={curImage} id="img360" alt="0"/>
+	    </div>
+    );
+  }
+}
+export default image360;
